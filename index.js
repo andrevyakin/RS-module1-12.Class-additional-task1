119e0add7d0c4d30f859628145ae6868fb777d8a
Андрей <andrevyakin@gmail.com>
2022-11-29 15:27:00 +0300

    Figured out the duplication.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 class Dictionary {
     #name
     #words
+
     constructor(name) {
         this.#name = name;
         this.#words = {};
@@ -38,28 +39,13 @@ class Dictionary {
 
 class HardWordsDictionary extends Dictionary {
 
-    /*add(word, description) {
-         super.add(word, description)
-        this.#_addNewWord(word, description)
-    }*/
-
-    // Как избавиться от дублирования кода?
-    // Если вызывать метод родительского класса через super (см. выше),
-    // то вызывается метод родительского класса #_addNewWord, а не дочернего. И в словарь не попадает isDifficult: true
-    // Если после super.add(word, description) вызвать дочерний метод путем this.#_addNewWord(word, description)
-    // то теряется вообще смысл вызова родительского метода add и не работает проверка на дублирование слов.
-    // Можно сделать addNewWord публичным, тогда через super все заработает, будет вызываться метод _addNewWord дочернего класса,
-    // но addNewWord должен быть приватным, по определению (он используется только внутри класса).
-    // Если сделать проверку на isDifficult в родительском классе, тогда я перетягиваю функциональность из дочернего класса
-    // в родительский. Что тоже в корне не правильно. Родительский класс ничего не должен знать о дочернем.
-    // А если переопределять add в дочернем классе, как сделал я, то это дублирование кода.
     add(word, description) {
-        if (!Object.keys(this.allWords).some(key => key === word))
-            this.#_addNewWord(word, description);
+        super.add(word, description)
+        this.#_addNewWord(word)
     }
 
-    #_addNewWord(wordKey, wordObj) {
-        this.allWords[wordKey] = {word: wordKey, description: wordObj, isDifficult: true};
+    #_addNewWord(wordKey) {
+        this.allWords[wordKey] = {...this.allWords[wordKey], isDifficult: true};
     }
 }
 
